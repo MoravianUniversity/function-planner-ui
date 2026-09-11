685422aa69139c91e5267b9c21cb49aca1f062f5
coderforlife <coderforlife@users.noreply.github.com>
2026-09-11 21:17:27 +0000

    deploy: 23d25dfd1ab52bb05bdc5f4ce341791ff7d22caa

---
+++ assets/madlibs-Bvvw7nWs.js
@@ -0,0 +1,1 @@
+import{i}from"./function-planner-nVbA2TN1.js";import"./ui-yMus3xAk.js";import"./yjs-DLZ4IDYl.js";window.addEventListener("DOMContentLoaded",()=>{i("planner","madlibs",{title:"Mad Libs",minFunctions:10,minTestable:2})});
